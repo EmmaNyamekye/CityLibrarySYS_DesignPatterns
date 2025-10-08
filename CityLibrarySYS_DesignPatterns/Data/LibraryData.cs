5594e94a1efc0bb3152f428db5885314be0ffef0
T00229159 <T00229159@S306-KY-2Y7Z894>
2025-10-08 16:15:12 +0100

    Add project files.

---
+++ CityLibrarySYS_DesignPatterns/Data/LibraryData.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CityLibrarySYS_DesignPatterns.Data
+{
+    public class LibraryData : DbContext
+    {
+        public LibraryData(DbContextOptions<LibraryData> options): base(options) { }
+
+        DbSet<Models.Library> Libraries { get; set; }
+
+    }
+}
---
+++ CityLibrarySYS_DesignPatterns/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CityLibrarySYS_DesignPatterns.Models
+{
+    public class Book
+    {
+        public string bookID { get; set; } = "";
+        public string ISBN { get; set; } = "";
+        public string title { get; set; } = "";
+        public string author { get; set; } = "";
+        public string genre { get; set; } = "";
+        public DateTime pubblication { get; set; } = DateTime.Now;
+        public string description { get; set; } = "";
+        public int LibraryID { get; set; }
+        public char status { get; set; }
+
+
+    }
+}
